ea09db356268af451f3fffc92e23585d29999a35
maherfauzi <azirariza9@gmail.com>
2025-03-16 17:40:09 +0700

    [add] local development server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("Hello World!")
+import ReactDOM from 'react-dom/client'
+
+  function App(){
+  return <h1>Hello Hello World</h1>;
+ }
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<App/>);
